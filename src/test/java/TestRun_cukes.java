3e80fcc9e7f9fffd5f6c9aa3a2a0a8cc0de28785
kphani <pkaliginadi@gmail.com>
2012-08-24 14:52:54 +0100

    add pretty report

--- src/test/java/TestRun_cukes.java
+++ src/test/java/TestRun_cukes.java
@@ -3,6 +3,6 @@ import cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = {"pretty", "html:target/cucumber"})
+@Cucumber.Options(format = {"pretty", "html:target/cucumber-html-report", "json-pretty:target/cucumber-report.json"})
 public class TestRun_cukes {
 }
